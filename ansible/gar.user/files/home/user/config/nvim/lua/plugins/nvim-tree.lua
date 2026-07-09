bf0c2556d336dab138a7c0171ca062bb842e66e9
gomesar <gomes.ar@protonmail.com>
2026-07-09 14:19:35 -0300

    feat(nvim): keymap w colapsa node atual no nvim-tree
    Antes só havia W, que colapsa a árvore inteira desde a raiz. Agora w
    chama api.node.collapse e fecha apenas o node sob o cursor.
    
    Reindenta o arquivo de tabs para espaços.

--- ansible/gar.user/files/home/user/config/nvim/lua/plugins/nvim-tree.lua
+++ ansible/gar.user/files/home/user/config/nvim/lua/plugins/nvim-tree.lua
@@ -1,34 +1,36 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "v1.17.0",
-	dependencies = {
-		"kyazdani42/nvim-web-devicons",
-	},
-	config = function()
-		local nvimtree = require("nvim-tree")
+    "nvim-tree/nvim-tree.lua",
+    version = "v1.17.0",
+    dependencies = {
+        "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+        local nvimtree = require("nvim-tree")
 
-		nvimtree.setup({
-			view = {
-				width = 40,
-			},
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-			git = {
-				ignore = false,
-			},
-		})
+        nvimtree.setup({
+            view = {
+                width = 40,
+            },
+            actions = {
+                open_file = {
+                    window_picker = {
+                        enable = false,
+                    },
+                },
+            },
+            git = {
+                ignore = false,
+            },
+        })
 
-		-- Set keymaps using custom function Keymap
-		Keymap("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
-		Keymap("n", "<leader>et", "<cmd>NvimTreeFindFile<CR>")
-		Keymap("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>")
-		Keymap("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
-		Keymap("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>")
-	end,
-	lazy = false,
+        local api = require("nvim-tree.api")
+        -- Set keymaps using custom function Keymap
+        Keymap("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
+        Keymap("n", "<leader>et", "<cmd>NvimTreeFindFile<CR>")
+        Keymap("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>")
+        Keymap("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
+        Keymap("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>")
+        Keymap("n", "w", api.node.collapse)
+    end,
+    lazy = false,
 }
